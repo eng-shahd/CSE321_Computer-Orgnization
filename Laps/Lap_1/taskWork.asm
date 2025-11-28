435c5ece58e97e77444159a9aeb43f4b646c4059
Shahd Mahmoud <158595641+eng-shahd@users.noreply.github.com>
2025-11-28 11:22:22 +0200

    Add user input prompts for name, ID, and course

--- Laps/Lap_1/taskWork.asm
+++ Laps/Lap_1/taskWork.asm
@@ -1,1 +1,38 @@
+.data
+name: .asciiz "Name :  "
+id: .asciiz "ID :  "
+course: .asciiz "Course :   "
+newLine: .asciiz "\n"
 
+
+.text
+main:
+
+
+     li $v0,4
+     la $a0,name
+     syscall
+
+     li $v0,5
+     syscall
+
+     li $v0,4
+     la $a0,id
+     syscall
+
+     li $v0,5
+     syscall
+
+     li $v0,4
+     la $a0,course
+     syscall
+
+     li $v0,5
+     syscall
+
+
+
+
+exit:
+     li $v0,10
+     syscall
